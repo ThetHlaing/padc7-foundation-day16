2389f6b0c568a7576b01a916b945310da4070ba7
thethlaing-mmdsgit config --global user.name thethlaing-mmds <thethlaing@mm-digital-solutions.com>
2019-07-28 09:21:17 +0630

    Home Work

--- 1.home-work-1.py
+++ 1.home-work-1.py
@@ -4,4 +4,13 @@
 #save the line into list by using append function
 
 
+file_path = input("Enter file path : ")
+handle = open(file_path,'r')
+content_by_list = list()
 
+for line in handle:
+    print("# ",line)
+    content_by_list.append(line)
+
+print("----------------")
+print(content_by_list)
--- 2.home-work-2.py
+++ 2.home-work-2.py
@@ -1,10 +1,15 @@
 # Create a function that return the largest number in the list
 
+def get_largest_number(item_list):
+    largest_number = 0
 
-def get_largest_number(input):
-    return input
+    for number in item_list:
+        if(largest_number < number):
+            largest_number = number
+            
+    return largest_number
 
-result = get_largest_number([1,2,3,4,5,12,1,6,99,2])
+result = get_largest_number([1,2,85,4,5,120,1,6,99,2])
 print(result) #99
 
 
